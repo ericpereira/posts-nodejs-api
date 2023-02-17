a04ad33e24aa4d9f32d9b1a674f68f424c9e0cca
Eric Pereira <eric.pereira@hotmail.com>
2023-02-17 18:45:24 -0300

    fix heroku migration error

--- knexfile.js
+++ knexfile.js
@@ -41,18 +41,19 @@ module.exports = {
   },
 
   production: {
-    client: 'postgresql',
+    client: 'mysql2',
     connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME
     },
     migrations: {
-      tableName: 'knex_migrations'
+      directory: path.resolve(__dirname, "src", "database", "migrations"),
+    },
+    seeds: {
+      directory: path.resolve(__dirname, "src", "database", "seeds"),
     }
   }
 
